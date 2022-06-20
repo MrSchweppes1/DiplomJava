d240f99b0f8993a2a46807d34325af1f5940fa8d
MrSchweppes1 <VusyVorobiov123@yandex.ru>
2022-06-20 22:37:53 +0300

    first

---
+++ app/src/main/java/com/example/diplomjava/ChecklistPage.java
@@ -0,0 +1,14 @@
+package com.example.diplomjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChecklistPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checklist_page);
+    }
+}
